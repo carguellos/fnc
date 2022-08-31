c73f97865a06c21cbcc200dda6845cc33ac70d29
Camilo Arguello <camilo.arguello@nauticadigital.co>
2022-08-30 20:53:28 -0500

    flask

--- app.py
+++ app.py
@@ -6,7 +6,35 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 @app.route('/')
 def home():
-    return render_template('layout.html')
+    return render_template('pages/home.html',
+        pagina={
+            "productos": [
+                    {
+                        "title":"Silla Nordik",
+                        "price":"$90.000"
+                    },
+                    {
+                        "title":"Silla Nordik",
+                        "price":"$110.000"
+                    },
+                    {
+                        "title":"Silla Nordik",
+                        "price":"$120.000"
+                    }
+            ]
+        }
+    )
+
+@app.route('/producto')
+def producto():
+    return render_template('pages/producto.html')
+
+
+@app.route('/catalogo')
+def catalogo():
+    return render_template('pages/catalogo.html')
+
+
 
 
 
